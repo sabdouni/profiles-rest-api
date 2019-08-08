5f5733d8e397d5cfa80a146cfc96fbf7dfd36126
Souhayl ABDOUNI <souhayl.abdouni@axa.fr>
2019-08-08 10:18:04 +0200

    feat(helloview): Add HelloApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    def get(self, request, format=None):
+        """
+        Returns a Hello World message
+        :param request: Http Request
+        :param format: Endpoint url format suffix
+        :return:
+        """
+
+        an_api_view = ["Hello World!"]
+
+        return Response({"an_api_view": an_api_view})
